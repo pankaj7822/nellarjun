9323fe78b135108ee71121a6dc3f6b388461d31e
pankaj7822 <pankaj.krjha.cse17@itbhu.ac.in>
2021-01-19 11:26:19 +0530

    Completed Models

---
+++ student/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Department,Complaint,Payment,Student
+
+admin.site.register(Department)
+
+@admin.register(Student)    
+class StudentAdmin(admin.ModelAdmin):
+    list_display=("name","current_semester","address","phone_number")
+    list_filter=("current_semester","branch")
+
+@admin.register(Complaint)    
+class ComplaintAdmin(admin.ModelAdmin):
+    list_display=("text","date_of_complaint","student")
+    list_filter=("date_of_complaint","student__name")
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display=("student","amount","date_of_payment","remarks")
+    list_filter=("student","date_of_payment")
---
+++ student/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+class Department(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=200,null=False,blank=False)
+    roll_no=models.CharField(max_length=50,null=True,blank=True)
+    date_of_joining=models.DateField(null=True,blank=True)
+    current_semester=models.CharField(max_length=50,null=True,blank=True)
+    branch=models.ForeignKey(Department,related_name="students",on_delete=models.CASCADE)
+    address=models.CharField(max_length=200,null=False,blank=False)
+    phone_number=models.CharField(max_length=200,null=False,blank=False)
+    father_name=models.CharField(max_length=200,null=False,blank=False)
+    
+    def __str__(self):
+        return self.name
+
+class Complaint(models.Model):
+    id=models.AutoField(primary_key=True)
+    text=models.CharField(max_length=200,null=False,blank=False)
+    date_of_complaint=models.DateField(null=True,blank=True)
+    student=models.ForeignKey(Student,related_name="complaints",on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.text+"_"+self.student.name
+    
+class Payment(models.Model):
+    id=models.AutoField(primary_key=True)
+    amount=models.CharField(max_length=10,null=False,blank=False)
+    date_of_payment=models.DateField(null=False,blank=False)
+    student=models.ForeignKey(Student,related_name="payments",on_delete=models.CASCADE)
+    remarks=models.CharField(max_length=500)
+    def __str__(self):
+        return self.student.name+"-"+self.amount
+
+    
+    
