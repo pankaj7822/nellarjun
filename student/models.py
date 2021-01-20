bfa944a2bc847d375758a1812d0d6b49c28401ac
pankaj7822 <pankaj.krjha.cse17@itbhu.ac.in>
2021-01-20 09:23:19 +0530

    Image Field

--- student/models.py
+++ student/models.py
@@ -17,6 +17,7 @@ class Student(models.Model):
     address=models.CharField(max_length=200,null=False,blank=False)
     phone_number=models.CharField(max_length=200,null=False,blank=False)
     father_name=models.CharField(max_length=200,null=False,blank=False)
+    image=models.CharField(max_length=200,null=True,blank=True)
     
     def __str__(self):
         return self.name
@@ -39,5 +40,4 @@ class Payment(models.Model):
     def __str__(self):
         return self.student.name+"-"+self.amount
 
-    
     
