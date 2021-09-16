c79a83ab87843ba2cd023446b1e1f266bc36181f
pankaj <pankaj.krjha.cse17@itbhu.ac.in>
2021-09-16 20:37:31 +0545

    Notices only

--- student/admin.py
+++ student/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from .models import Department,Complaint,Payment,Student,Notice
 
-admin.site.register(Department)
+# admin.site.register(Department)
 admin.site.register(Notice)
 
-@admin.register(Student)    
-class StudentAdmin(admin.ModelAdmin):
-    list_display=("name","current_semester","address","phone_number")
-    list_filter=("current_semester","branch")
+# @admin.register(Student)    
+# class StudentAdmin(admin.ModelAdmin):
+#     list_display=("name","current_semester","address","phone_number")
+#     list_filter=("current_semester","branch")
 
-@admin.register(Complaint)    
-class ComplaintAdmin(admin.ModelAdmin):
-    list_display=("text","date_of_complaint","student")
-    list_filter=("date_of_complaint","student__name")
+# @admin.register(Complaint)    
+# class ComplaintAdmin(admin.ModelAdmin):
+#     list_display=("text","date_of_complaint","student")
+#     list_filter=("date_of_complaint","student__name")
 
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display=("student","amount","date_of_payment","remarks")
-    list_filter=("student","date_of_payment")
+# @admin.register(Payment)
+# class PaymentAdmin(admin.ModelAdmin):
+#     list_display=("student","amount","date_of_payment","remarks")
+#     list_filter=("student","date_of_payment")
